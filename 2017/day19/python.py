d396c7c96fc4ccff99c11e36861aeedd80aca0ca
John Pribyl <johnmpribyl@gmail.com>
2025-10-21 17:02:12 -0500

    2017 day 19 and 2024 day 12

---
+++ 2017/day19/python.py
@@ -0,0 +1,55 @@
+def main():
+    input = open('/home/john/Documents/Projects/advent_of_code/2017/day19/input.txt', 'r').readlines()
+
+    current_row = 0
+    current_column = 0
+    current_char = '|'
+    current_direction = 'down'
+    part1_result = []
+    part2_result = 0
+
+
+    # find first pipe
+    current_column = input[0].find('|')
+
+    while current_char != ' ': # Path ends when we land on a blank space
+        current_char = input[current_row][current_column]
+        # Check for letter
+        if current_char.isalpha():
+            part1_result.append(current_char)
+        # Check for corner
+        elif current_char == '+':
+            # changing direction
+            if current_direction == 'up' or current_direction == 'down':
+                # If we were going up or down, we should now go either left or right
+                left = input[current_row][current_column - 1]
+                right = input[current_row][current_column + 1]
+                if left != ' ':
+                    current_direction = 'left'
+                elif right != ' ':
+                    current_direction = 'right'
+            elif current_direction == 'left' or current_direction == 'right':
+                # If we were going left or right, we should now go either up or down
+                up = input[current_row - 1][current_column]
+                down = input[current_row + 1][current_column]
+                if up != ' ':
+                    current_direction = 'up'
+                elif down != ' ':
+                    current_direction = 'down'
+
+        # Take one step in current direction
+        if current_direction == 'down':
+            current_row += 1
+        elif current_direction == 'up':
+            current_row -= 1
+        elif current_direction == 'right':
+            current_column += 1
+        elif current_direction == 'left':
+            current_column -= 1
+
+        part2_result += 1
+
+    print("".join(part1_result)) # Part 1
+    print(part2_result - 1) # Part 2
+
+main()
---
+++ 2024/day12/python.py
@@ -0,0 +1,147 @@
+from collections import deque
+
+def main():
+    example1_input_path = '/home/john/Documents/Projects/advent_of_code/2024/day12/input/example1.txt'
+    example2_input_path = '/home/john/Documents/Projects/advent_of_code/2024/day12/input/example2.txt'
+    example3_input_path = '/home/john/Documents/Projects/advent_of_code/2024/day12/input/example3.txt'
+    example4_input_path = '/home/john/Documents/Projects/advent_of_code/2024/day12/input/example4.txt'
+    example5_input_path = '/home/john/Documents/Projects/advent_of_code/2024/day12/input/example5.txt'
+    part1_input_path = '/home/john/Documents/Projects/advent_of_code/2024/day12/input/input.txt'
+
+    part2(example1_input_path) # Correct answer: Perimeter - 140,     Side - 80
+    part2(example2_input_path) # Correct answer: Perimeter - 772,     Side - 436
+    part2(example3_input_path) # Correct answer: Perimeter - 1930,    Side - NA
+    part2(example4_input_path) # Correct answer: Perimeter - NA,      Side - 236
+    part2(example5_input_path) # Correct answer: Perimeter - NA,      Side - 368
+    part2(part1_input_path)    # Correct answer: Perimeter - 1457298, Side - 921636
+
+def part2(input_path):
+    # Input is essentially a graph with many subgraphs
+    # Non-contiguous subgraphs may have the same letter, but should be treated separately
+    # E.g. there may be two 'A' subgraphs that are not connected. They should be treated individually
+    # BFS search to visit unseen nodes and traverse their subgraphs 
+    input = open(input_path, 'r').readlines()
+    num_rows = len(input)
+    num_cols = len(input[0]) - 1
+
+    seen = {}
+    total_perimeter_cost = 0
+    total_side_cost = 0
+    for row_idx, row in enumerate(input):
+        row = row.rstrip()
+        for col_idx in range(num_rows):
+            if (row_idx, col_idx) not in seen:
+                (perimeter_cost, side_cost) = traverse_subgraph(input, row_idx, num_rows, col_idx, num_cols, seen)
+                total_perimeter_cost += perimeter_cost
+                total_side_cost += side_cost
+    print(f"Perimeter Cost: {total_perimeter_cost}, Side cost: {total_side_cost}")
+
+def traverse_subgraph(input, row_idx, num_rows, col_idx, num_cols, seen):
+    to_visit = deque()
+    to_visit.append((row_idx, col_idx))
+    subgraph_area = 0
+    subgraph_perimeter = 0
+    subgraph_corners = 0
+
+    # BFS to traverse the subgraph
+    while len(to_visit) > 0:
+        (row_idx, col_idx) = to_visit.popleft()
+        # Check if we've already visited
+        if (row_idx, col_idx) in seen:
+            continue
+
+        seen[(row_idx, col_idx)] = True
+        this_plot_letter = input[row_idx][col_idx]
+        subgraph_area += 1
+        plot_neighbors = 0
+
+        # check up for neighbor
+        up = ''
+        if row_idx > 0:
+            up = input[row_idx - 1][col_idx] 
+            if up == this_plot_letter:
+                plot_neighbors += 1
+                if (row_idx - 1, col_idx) not in seen:
+                    to_visit.append((row_idx - 1, col_idx))
+            else:
+                up = ''
+
+        # check down for neighbor
+        down = ''
+        if row_idx < num_rows - 1:
+            down = input[row_idx + 1][col_idx] 
+            if down == this_plot_letter:
+                plot_neighbors += 1
+                if (row_idx + 1, col_idx) not in seen:
+                    to_visit.append((row_idx + 1, col_idx))
+            else:
+                down = ''
+        
+        # check left
+        left = ''
+        if col_idx > 0:
+            left = input[row_idx][col_idx - 1] 
+            if left == this_plot_letter:
+                plot_neighbors += 1
+                if (row_idx, col_idx - 1) not in seen:
+                    to_visit.append((row_idx, col_idx - 1))
+            else:
+                left = ''
+
+        # check right
+        right = ''
+        if col_idx < num_cols - 1:
+            right = input[row_idx][col_idx + 1] 
+            if right == this_plot_letter:
+                plot_neighbors += 1
+                if (row_idx, col_idx + 1) not in seen:
+                    to_visit.append((row_idx, col_idx + 1))
+            else:
+                right = ''
+
+        # look for "outside" corners
+        #    _
+        #   | 
+        #
+        if (not up and not left):
+            subgraph_corners += 1
+        if (not up and not right):
+            subgraph_corners += 1
+        if (not down and not left):
+            subgraph_corners += 1
+        if (not down and not right):
+            subgraph_corners += 1
+
+        # look for "inside" corners
+        #
+        #      |_
+        #       
+        if (up and left) and not input[row_idx - 1][col_idx - 1] == this_plot_letter:
+            subgraph_corners += 1
+        if (up and right) and not input[row_idx - 1][col_idx + 1] == this_plot_letter:
+            subgraph_corners += 1
+        if (down and left) and not input[row_idx + 1][col_idx - 1] == this_plot_letter:
+            subgraph_corners += 1
+        if (down and right) and not input[row_idx + 1][col_idx + 1] == this_plot_letter:
+            subgraph_corners += 1
+        plot_edges = 4 - plot_neighbors
+        subgraph_perimeter += plot_edges
+
+    perimeter_cost = subgraph_area * subgraph_perimeter
+    side_cost = subgraph_area * subgraph_corners
+    return (perimeter_cost, side_cost)
+            
+
+
+            
+def get_plot_data(plot_data, total_cost):
+    num_regions = len(plot_data.keys())
+    total_area = 0
+    for plot_key in plot_data.keys():
+        (plot_area, plot_perimeter) = plot_data[plot_key]
+        total_area += plot_area
+        total_cost += plot_area * plot_perimeter
+
+    return (num_regions, total_area, total_cost)
+
+main()
